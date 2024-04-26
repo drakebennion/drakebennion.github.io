5524bc2c8c04bd980d33c924ebd8bc58c96516e0
Drake <drake.bennion@gmail.com>
2024-04-26 13:25:26 -0600

    fix: redraw on resize

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
 const canvas = document.getElementById("canvas");
-const canvasHeight = document.documentElement.scrollHeight;
-const canvasWidth = document.documentElement.scrollWidth;
 const colors = ['#E5625E', '#C3BEF7', '#63E2C6', '#E5E059', '#595457', '#FFF'];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-// todo: recalculate all this on resize? for responsiveness hmmmmmm
-canvas.height = canvasHeight;
-canvas.width = canvasWidth;
-const ctx = canvas.getContext('2d');
-
-// get 10 equal width squares...will I adjust it hmmmmmmmmmmmmm
-// is canvas responsive? can i make it so hmhmhm
-const numCols = 15;
-const squareSideLength = canvasWidth / numCols;
-const numRows = canvasHeight / squareSideLength;
-for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-        ctx.fillStyle = colors[getRandomInt(colors.length)];
-        ctx.fillRect(col * squareSideLength, row * squareSideLength, squareSideLength, squareSideLength);
-    }
+window.onload = function() {
+    initCanvas();
 }
 
+window.onresize = function() {
+    // todo: this works :) but if you shrink screen then expand, more space continually added
+    // below...figure that out soon :) 
 
-const lastXY = [-1, -1];
-canvas.onmousemove = function(event) {
-    const rect = this.getBoundingClientRect(),
-    x = event.clientX - rect.left,
-    y = event.clientY - rect.top; 
+    // also might be nice to wait for resize to be done before recalculating hmm
+    initCanvas();
+}
 
-    const squareX = squareSideLength * Math.floor(x / squareSideLength);
-    const squareY = squareSideLength * Math.floor(y / squareSideLength);
+function initCanvas() {
+    const canvasHeight = document.documentElement.scrollHeight;
+    const canvasWidth = document.documentElement.scrollWidth;
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
+    const ctx = canvas.getContext('2d');
 
-    if (!(lastXY[0] === squareX && lastXY[1] === squareY)) {
-        ctx.fillStyle = colors[getRandomInt(colors.length)];
-        ctx.fillRect(squareX, squareY, squareSideLength, squareSideLength);
-        lastXY[0] = squareX;
-        lastXY[1] = squareY;
+    const numCols = 25; // todo: higher number for wider screens
+    const squareSideLength = canvasWidth / numCols;
+    const numRows = canvasHeight / squareSideLength;
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+            ctx.fillStyle = colors[getRandomInt(colors.length)];
+            ctx.fillRect(col * squareSideLength, row * squareSideLength, squareSideLength, squareSideLength);
+        }
+    }
+
+
+    const lastXY = [-1, -1];
+    canvas.onmousemove = function(event) {
+        const rect = this.getBoundingClientRect(),
+        x = event.clientX - rect.left,
+        y = event.clientY - rect.top; 
+
+        const squareX = squareSideLength * Math.floor(x / squareSideLength);
+        const squareY = squareSideLength * Math.floor(y / squareSideLength);
+
+        if (!(lastXY[0] === squareX && lastXY[1] === squareY)) {
+            ctx.fillStyle = colors[getRandomInt(colors.length)];
+            ctx.fillRect(squareX, squareY, squareSideLength, squareSideLength);
+            lastXY[0] = squareX;
+            lastXY[1] = squareY;
+        }
     }
 }
